dd1890e2e9d827603abdd6e66f9f1011e8073a88
tshl <1320528371@qq.com>
2023-02-14 01:52:11 +0800

    第二次提交

--- src/main/java/com/example/demo140/Demo140Application.java
+++ src/main/java/com/example/demo140/Demo140Application.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo140Application {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(Demo140Application.class, args);
     }
